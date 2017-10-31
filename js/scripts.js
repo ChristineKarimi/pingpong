faf8a8c319762a92de79e1b0b5a67b9c6dc027b9
christine karimi <karimichristine@ymail.com>
2017-10-31 10:10:21 +0300

    saved some changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,4 @@
-// //business logic
-// var userNumber;
-//
-// var numberArr = [];
-//
-// var pingPongFunc = function (number) {
-//     for (var i = 1; i <= number; i++) {
-//         if (i % 15 === 0) {
-//             numberArr.push("pingpong");
-//         } else if (i % 5 === 0) {
-//             numberArr.push("pong");
-//         } else if (i % 3 === 0) {
-//             numberArr.push("ping");
-//         } else {
-//             numberArr.push(i);
-//         }
-//     }
-// };
-// //userinterface mate
-// $(document).ready(function () {
-//     $('form#pingpong').submit(function (event) {
-//         event.preventDefault();
-//         var number = parseInt($('input#inputNumber').val());
-//         $('#results').text(" ");
-//         pingPongFunc(number);
-//         numberArr.forEach(function (number) {
-//             $('#results').append('<li>' + number + '<li>');
-//         });
-//     });
-// });
+
 
 $(document).ready(function(){
   var userinput = $("#inputNumber").val();
@@ -51,7 +22,7 @@ $(document).ready(function(){
 
 
     }else{
-      warning.text("Yo.Human Add some shit on that input field");
+      warning.text(" Add something in that input field");
     }
     event.preventDefault()
   });
